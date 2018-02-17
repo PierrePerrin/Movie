d1e63f32460897939b60748f0f6865e09cb71ee7
Pierre <pierrepp92@gmail.com>
2018-02-17 18:37:33 +0100

    Add fastlane

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,7 @@
+var appIdentifier: String { return "PP.Movie" } // The bundle identifier of your app
+var appleID: String { return "pierrepp92@hotmail.fr" } // Your Apple email address
+
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
